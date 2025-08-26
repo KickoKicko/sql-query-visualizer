5c5f9e42d5f6462f58f20f9450829b5926997475
NikolajGoj <kickokicko110@gmail.com>
2025-08-26 21:36:34 +0200

    link backend and frontend

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/test', methods = ["POST"])
+def test():
+    print("hey")
+    query = {"rows": ["Row 1"], "query":"SELECT * FROM test;"}
+    print(jsonify(query))
+    return jsonify(query)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
