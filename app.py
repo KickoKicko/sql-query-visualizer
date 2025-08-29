83e21282d806788e9e94446a477a7aa1f93b065d
NikolajGoj <kickokicko110@gmail.com>
2025-08-29 17:05:41 +0200

    add tests and start of parser

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 
 
 app = Flask(__name__)
@@ -8,12 +8,30 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/test', methods = ["POST"])
-def test():
-    print("hey")
-    query = {"rows": ["Row 1"], "query":"SELECT * FROM test;"}
-    print(jsonify(query))
-    return jsonify(query)
+@app.route('/query', methods = ["POST"])
+def query():
+    #print(request.data.decode("utf-8"))
+    query = request.data.decode("utf-8")
+    query = query[10:len(query)-2]
+    query =query =  tests(query)
+    words = query.split()
+    table_part = []
+    for i in range(len(words)):
+        if words[i] == "FROM":
+            table_part = words[i+1:]
+    print(query)
+    print(words)
+    print(table_part)
+    query_test = {"rows": ["Row 1"], "query":"SELECT * FROM test;"}
+    return jsonify(query_test)
+
+
+
+def tests(query):
+    match query:
+        case "test1": return "SELECT * FROM table"
+        case "test2": return "SELCET Id,Name,Password FROM Users"
+    return query
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
